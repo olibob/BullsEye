08a36024e463874619a21dd495fb74baa92c1df4
Olibob <robby57@gmail.com>
2014-09-25 15:55:08 +0200

    Add some slider action ;-)

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,24 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBAction func showAlert(sender: UIButton) {
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my alert message", preferredStyle: UIAlertControllerStyle.Alert)
-        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
-        alert.addAction(action)
+    
+  var currentValue: Int = 0
+
+  @IBOutlet weak var bullSlider: UISlider!
+  
+  @IBAction func showAlert(sender: UIButton) {
+    let alert = UIAlertController(title: "\(currentValue)", message: "The value of the slider is: \(currentValue)", preferredStyle: UIAlertControllerStyle.Alert)
+    let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+    alert.addAction(action)
         
-        self.presentViewController(alert, animated: true, completion: nil)
+    self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(sender: UISlider) {
+      currentValue = lroundf((sender.value))
     }
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+      currentValue = lroundf(bullSlider.value)
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
     }
-
 
 }
 

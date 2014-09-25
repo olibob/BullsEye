31bf24dcd3abab61115ac3249f0a311e7c0b2592
Olibob <robby57@gmail.com>
2014-09-25 18:40:36 +0200

    Switch to landscape

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,31 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     
   var currentValue: Int = 0
+  var randomNumber: Int = 0
 
   @IBOutlet weak var bullSlider: UISlider!
+  @IBOutlet weak var bullRandomNumberLabel: UILabel!
   
   @IBAction func showAlert(sender: UIButton) {
     let alert = UIAlertController(title: "\(currentValue)", message: "The value of the slider is: \(currentValue)", preferredStyle: UIAlertControllerStyle.Alert)
     let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
     alert.addAction(action)
-        
-    self.presentViewController(alert, animated: true, completion: nil)
-    }
     
-    @IBAction func sliderMoved(sender: UISlider) {
-      currentValue = lroundf((sender.value))
-    }
+    self.presentViewController(alert, animated: true, completion: nil)
+  }
+  
+  @IBAction func sliderMoved(sender: UISlider) {
+    currentValue = lroundf((sender.value))
+  }
 
-    override func viewDidLoad() {
-      super.viewDidLoad()
-      // Do any additional setup after loading the view, typically from a nib.
-      currentValue = lroundf(bullSlider.value)
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    currentValue = lroundf(bullSlider.value)
+    randomNumber = generateRandomNumber()
+    bullRandomNumberLabel.text = "\(randomNumber)"
+  }
 
-    override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-    }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func generateRandomNumber() -> Int {
+    return Int(arc4random_uniform(100) + 1)
+  }
 
 }
 

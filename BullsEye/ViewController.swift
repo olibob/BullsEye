0acf7e7de9a90c8635c7774d45a662e486b9b080
olibob <robby57@gmail.com>
2014-09-26 16:00:54 +0200

    Finalise game play

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -11,14 +11,43 @@ import UIKit
 class ViewController: UIViewController {
     
   var currentValue: Int = 0
-  var randomNumber: Int = 0
+  var targetValue: Int = 0
+  var score: Int = 0
+  var round: Int = 0
 
   @IBOutlet weak var bullSlider: UISlider!
   @IBOutlet weak var bullRandomNumberLabel: UILabel!
+  @IBOutlet weak var bullScore: UILabel!
+  @IBOutlet weak var bullRound: UILabel!
   
   @IBAction func showAlert(sender: UIButton) {
-    let alert = UIAlertController(title: "\(currentValue)", message: "The value of the slider is: \(currentValue)", preferredStyle: UIAlertControllerStyle.Alert)
-    let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+    // Calculate difference between current value and target value
+    let difference: Int = abs(currentValue - targetValue)
+    var points = 100 - difference
+    
+    var title = ""
+    if difference == 0 {
+      title = "Incredible! Spot on!"
+      points += 100      // Additional 100 points when the user hits the target value
+    } else if difference <= 3 {
+      title = "Wow, that was close!"
+      if difference == 1 {
+        points += 50
+      }
+    } else if difference <= 6 {
+      title = "Pretty good!"
+    } else if difference <= 10 {
+      title = "Not bad"
+    } else {
+      title = "mmm, you can do better ;-)"
+    }
+    
+    score += points
+    
+    let alert = UIAlertController(title: title, message: "You scored \(points)", preferredStyle: UIAlertControllerStyle.Alert)
+    let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { action in
+      self.startNewRound()
+    })
     alert.addAction(action)
     
     self.presentViewController(alert, animated: true, completion: nil)
@@ -28,12 +57,17 @@ class ViewController: UIViewController {
     currentValue = lroundf((sender.value))
   }
 
+  @IBAction func startOver(sender: UIButton) {
+    round = 0
+    score = 0
+    startNewRound()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-    currentValue = lroundf(bullSlider.value)
-    randomNumber = generateRandomNumber()
-    bullRandomNumberLabel.text = "\(randomNumber)"
+    
+    startNewRound()
   }
 
   override func didReceiveMemoryWarning() {
@@ -43,6 +77,23 @@ class ViewController: UIViewController {
   
   func generateRandomNumber() -> Int {
     return Int(arc4random_uniform(100) + 1)
+  }
+  
+  func startNewRound() {
+    // Generate a new target number to find
+    targetValue = generateRandomNumber()
+    currentValue = 50
+    round++
+    
+    updateLabels()
+    
+//    bullSlider.value = Float(currentValue)
+  }
+  
+  func updateLabels() {
+    bullRandomNumberLabel.text = "\(targetValue)"
+    bullScore.text = "\(score)"
+    bullRound.text = "\(round)"
   }
 
 }
